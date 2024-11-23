110ebf4e96993ef90be314d5f5aeb3e18749e68d
XIAOXIAO <xiaoxiao@xiaoxiaebookpro.home>
2024-11-22 20:50:16 -0500

    1

--- virus.cc
+++ virus.cc
@@ -2,7 +2,3 @@
 
 Virus::Virus(int row, int col, int strength, char name, Player* owner, bool displayed, bool doublespeed, bool locked) : 
     Link{row, col, strength, name, owner, displayed, doublespeed, locked} {}
-
-int Virus::getStrength() { return strength; }
-
-void Virus::setStrength(int strength, int row, int col) { this->strength = strength; }
