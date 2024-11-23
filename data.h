d9a7a5603422597f5bc287376f7faea08ff3c639
XIAOXIAO <xiaoxiao@xiaoxiaebookpro.home>
2024-11-22 19:50:05 -0500

    public ctor

--- data.h
+++ data.h
@@ -2,8 +2,8 @@
 
 class Data: public Link {
     int strength;
-    Data (int row, int col, int strength, char name, Player* owner, bool displayed, bool doublespeed, bool locked);
   public:
+    Data (int row, int col, int strength, char name, Player* owner, bool displayed, bool doublespeed, bool locked);
     int getStrength() override {};
     void setStrength(int strength, int row, int col) override {};
 };
--- virus.h
+++ virus.h
@@ -2,8 +2,8 @@
 
 class Virus: public Link {
     int strength;
-    Virus (int row, int col, int strength, char name, Player* owner, bool displayed, bool doublespeed, bool locked);
   public:
+    Virus (int row, int col, int strength, char name, Player* owner, bool displayed, bool doublespeed, bool locked);
     int getStrength() override {};
     void setStrength(int strength, int row, int col) override {};
 };
