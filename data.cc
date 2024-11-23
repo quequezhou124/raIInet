a6f894ee64c1720c72311cff5558ba39e400f8fc
XIAOXIAO <xiaoxiao@xiaoxiaebookpro.home>
2024-11-22 21:22:45 -0500

    ctor

--- data.cc
+++ data.cc
@@ -1,4 +1,4 @@
 #include "data.h"
 
-Data::Data(int row, int col, int strength, char name, Player* owner, bool displayed, bool doublespeed, bool locked) : 
+Data::Data(int row, int col, int strength, char name, Player* owner, bool displayed = false, bool doublespeed = false, bool locked = false) : 
     Link{row, col, strength, name, owner, displayed, doublespeed, locked} {}
