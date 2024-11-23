99a84d04061a2549848bc03ae2b47ce5c4b61561
XIAOXIAO <xiaoxiao@xiaoxiaebookpro.home>
2024-11-22 23:55:58 -0500

    1

--- unit.h
+++ unit.h
@@ -14,7 +14,7 @@ class Unit {
     int getRow() const { return row; }
     int getCol() const { return col; }
     virtual int getStrength() = 0;
-    virtual std::string getType ();
+    virtual std::string getType () = 0;
     virtual void setStrength(int strength, int row, int col) = 0;
     virtual void setrow(int row) = 0;
     virtual void setcol(int col) = 0;
