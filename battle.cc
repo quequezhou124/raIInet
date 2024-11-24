ae47fa3e398194df2c3de999e19b208f91329355
XIAOXIAO <xiaoxiao@xiaoxiaebookpro.home>
2024-11-23 21:46:48 -0500

    ye

---
+++ battle.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+#include <algorithm>
+#include "board.h"
+#include "blank.h"
+#include "subject.h"
+#include "player.h"
+#include "textObserver.h"
+#include "unit.h"
+#include "link.h"
+#include "data.h"
+#include "virus.h"
+#include "decorator.h"
+#include "datadecorator.h"
+#include "virusdecorator.h"
+#include <vector>
+
+bool battle (Link *l1, Link *l2, Player *p1,Player *p2) {
+    if (l1->getStrength() >= l2->getStrength()) {
+        if (dynamic_cast<Data*>(l2)) {
+            p1->setdownloadD(p1->getdownloadD()+1);
+        }
+    }
+} 
