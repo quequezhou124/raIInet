e22e34ffe2fb1611e46c4b4af6b8f9fa2b7dfa3f
XIAOXIAO <xiaoxiao@xiaoxiaebookpro.home>
2024-11-23 21:54:44 -0500

    move fix

--- battle.cc
+++ battle.cc
@@ -15,10 +15,14 @@
 #include "virusdecorator.h"
 #include <vector>
 
-bool battle (Link *l1, Link *l2, Player *p1,Player *p2) {
+bool battle (Link *l1, Link *l2, Player *p1,Player *p2, Board *board) {
     if (l1->getStrength() >= l2->getStrength()) {
         if (dynamic_cast<Data*>(l2)) {
             p1->setdownloadD(p1->getdownloadD()+1);
+        } else if (dynamic_cast<Virus*>(l2)) {
+            p1->setdownloadV(p1->getdownloadV()+1);
         }
+
+        
     }
 } 
