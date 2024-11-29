e4bd3376ef11db242f54a7f7bd797a1eb2b554d1
DariaJiang <jiangjiajie0510@gmail.com>
2024-11-28 23:31:56 -0500

    main.cc

--- graphicObserver.h
+++ graphicObserver.h
@@ -39,46 +39,53 @@ public:
             for (int j = 0; j < 8; ++j) {
                 int x = j * cellSize;
                 int y = i * cellSize;
+                char c = subject->getState(i, j);
 
                 char state = subject->getState(i, j);
                 bool isPlayer1Turn = curplayer->isplayer1turn();
                 if (subject->getBoard()->getFirewall(i, j, curplayer) && subject->getBoard()->getFirewall(i, j, curplayer)->getName() == 'w' && curplayer->isplayer1turn()) {
                     window.fillRectangle(x, y, cellSize, cellSize, Xwindow::White); 
-                    window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Yellow); 
+                    window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Orange); 
                 } else if (subject->getBoard()->getFirewall(i, j, otherplayer) && subject->getBoard()->getFirewall(i, j, otherplayer)->getName() == 'm' && !curplayer->isplayer1turn()) {
                     window.fillRectangle(x, y, cellSize, cellSize, Xwindow::White);
                     window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Orange);
                 } else if (state == 'S') {
-                    window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Blue);
+                    window.fillRectangle(x, y, cellSize, cellSize, Xwindow::LightBlue);
+                    window.drawString(j * 100 + 5, i * 100 + 40, std::format("Sever Pot: ({},{})", i, j));
                     window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Black);
                 } else if (state >= 'a' && state <= 'z') {
                     if(curplayer->isplayer1turn()){
                         if (subject->getBoard()->getUnit(i, j)->getType() == "D"&&subject->getBoard()->getUnit(i, j)->getDownloaded()==false) {
                             window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Green);
                             window.drawString(x + cellSize / 4, y + cellSize / 2, std::string(1, state), Xwindow::Black);
+                            window.drawString(j * 100 + 5, i * 100 + 40, std::format("Data {}: ({},{})", c, i, j));
                             window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Black);
                         } else if (subject->getBoard()->getUnit(i, j)->getType() == "V"&&subject->getBoard()->getUnit(i, j)->getDownloaded()==false) {
                             window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Red);
+                            window.drawString(j * 100 + 5, i * 100 + 40, std::format("Virus {}: ({},{})", c, i, j));
                             window.drawString(x + cellSize / 4, y + cellSize / 2, std::string(1, state), Xwindow::Black);
                             window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Black);
                         }
                     } else {
-                        window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Black);
-                        window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Black);
+                        window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Gray);
+                        window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::White);
                     }
                 } else if (state >= 'A' && state <= 'Z') {
                     if(!curplayer->isplayer1turn()){
                         if (subject->getBoard()->getUnit(i, j)->getType() == "D" &&subject->getBoard()->getUnit(i, j)->getDownloaded()==false) {
-                                window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Pink);
+                                window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Green);
+                                window.drawString(j * 100 + 5, i * 100 + 40, std::format("Data {}: ({},{})", c, i, j));
                                 window.drawString(x + cellSize / 4, y + cellSize / 2, std::string(1, state), Xwindow::Black);
                                 window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Black);
                         } else if (subject->getBoard()->getUnit(i, j)->getType() == "V"&&subject->getBoard()->getUnit(i, j)->getDownloaded()==false) {
-                                window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Gray);
+                                window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Red);
                                 window.drawString(x + cellSize / 4, y + cellSize / 2, std::string(1, state), Xwindow::Black);
+                                window.drawString(j * 100 + 5, i * 100 + 40, std::format("Virus {}: ({},{})", c, i, j));
                                 window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Black);
                         }
                     } else {
-                        window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Black);
+                        window.fillRectangle(x, y, cellSize, cellSize, Xwindow::Gray);
+                        window.drawString(x + cellSize / 4, y + cellSize / 2, std::string(1, state), Xwindow::Black);
                         window.drawRectangle(x, y, cellSize, cellSize, borderWidth, Xwindow::Black);
                     }
                 } else {
